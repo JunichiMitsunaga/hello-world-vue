5ab079e789f6f89ac6963203ba68ee607b61e89f
Toyosea <46737875+Toyosea@users.noreply.github.com>
2020-10-27 17:03:34 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Hell"/>
+    <HelloWorld msg="Welcome to Paradise"/>
   </div>
 </template>
 
@@ -24,6 +24,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-  background-color: red;
 }
 </style>
